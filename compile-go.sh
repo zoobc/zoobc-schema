201d583c8643a00510336de8480e2977c36575e6
Gede Sukra Widhyawan <g.sukrawidhyawan02@gmail.com>
2019-07-04 16:21:02 +0800

    Add Block Model & Service (#1)
    * add block model & service
    
    * change field : BlocksmithAddress to BlocksmithID
    
    * add GetBlock service

---
+++ compile-go.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+protoc -I${PWD} ${PWD}/model/*.proto --go_out=plugins=grpc:$GOPATH/src
+protoc -I${PWD} -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --grpc-gateway_out=logtostderr=true:${GOPATH}/src ${PWD}/service/*.proto --swagger_out=logtostderr=true:${PWD} --go_out=plugins=grpc:$GOPATH/src
